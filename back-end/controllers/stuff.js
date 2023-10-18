f779671c4eb24af5f5cb3a9261754d09e7e6c16f
Myokatoshi <sylvain.motta@gmail.com>
2023-10-18 13:24:03 +0200

    fix like/dislike

--- back-end/controllers/stuff.js
+++ back-end/controllers/stuff.js
@@ -2,6 +2,66 @@ const fs = require('fs');
 const Thing = require('../models/Thing');
 
 exports.likeOrDislikeSauce = (req, res, next) => {
+  const likeSauce = req.body.like;
+  const authenticatedUserId = req.auth.userId; // L'ID de l'utilisateur authentifié "res.locals.userId"(autre facon de l'écrire)
+
+  Thing.findOne({ _id: req.params.id })
+      .then(sauce => {
+          const userAlreadyLiked = sauce.usersLiked.includes(authenticatedUserId);
+          const userAlreadyDisliked = sauce.usersDisliked.includes(authenticatedUserId);
+
+          if (likeSauce === 1) {
+              if (userAlreadyLiked) {
+                  console.log("Sauce déjà likée");
+              } else if (userAlreadyDisliked) {
+                  // Si l'utilisateur avait déjà "disliké", mettez à jour pour "liker" à la place.
+                  sauce.usersLiked.push(authenticatedUserId);
+                  sauce.likes += 1;
+                  sauce.usersDisliked.pull(authenticatedUserId);
+                  sauce.dislikes -= 1;
+                  console.log("Avis like pris en compte (mis à jour)");
+              } else {
+                  sauce.usersLiked.push(authenticatedUserId);
+                  sauce.likes += 1;
+                  console.log("Avis like bien pris en compte");
+              }
+          } else if (likeSauce === -1) {
+              if (userAlreadyDisliked) {
+                  console.log("Sauce déjà dislikée");
+              } else if (userAlreadyLiked) {
+                  // Si l'utilisateur avait déjà "liké", mettez à jour pour "disliker" à la place.
+                  sauce.usersDisliked.push(authenticatedUserId);
+                  sauce.dislikes += 1;
+                  sauce.usersLiked.pull(authenticatedUserId);
+                  sauce.likes -= 1;
+                  console.log("Avis dislike pris en compte (mis à jour)");
+              } else {
+                  sauce.usersDisliked.push(authenticatedUserId);
+                  sauce.dislikes += 1;
+                  console.log("Avis dislike bien pris en compte");
+              }
+          } else {
+              // Retirer le like ou le dislike de l'utilisateur
+              if (userAlreadyLiked) {
+                  sauce.usersLiked.pull(authenticatedUserId);
+                  sauce.likes -= 1;
+              }
+              if (userAlreadyDisliked) {
+                  sauce.usersDisliked.pull(authenticatedUserId);
+                  sauce.dislikes -= 1;
+              }
+          }
+
+          Thing.updateOne({ _id: req.params.id }, sauce)
+              .then(() => res.status(200).json({ message: 'Avis bien pris en compte !' }))
+              .catch(error => res.status(400).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
+};
+
+
+
+/*exports.likeOrDislikeSauce = (req, res, next) => {
     const likeSauce = req.body.like;
     Thing.findOne({ _id: req.params.id })
       .then(sauce => {
@@ -13,12 +73,12 @@ exports.likeOrDislikeSauce = (req, res, next) => {
           console.log("dans le ELSE");
           sauce.usersLiked.push(req.auth.userId);
           sauce.likes += 1;
-          console.log("avis like bien ris en compte");
+          console.log("avis like bien pris en compte");
           }
         } else if(likeSauce < 0) {
           sauce.usersDisliked.push(req.auth.userId);
           sauce.dislikes += 1;
-          console.log("avis dislike bien ris en compte");
+          console.log("avis dislike bien pris en compte");
         } else {
             sauce.usersLiked.forEach((userLiked, index) => {
               if(userLiked = req.auth.userId) {
@@ -38,7 +98,7 @@ exports.likeOrDislikeSauce = (req, res, next) => {
           .catch(error => res.status(400).json({ error }));
       })
       .catch(error => res.status(500).json({ error }));      
-  };
+  };*/
   
   exports.createThing = (req, res, next) => {
     const thingObject = JSON.parse(req.body.sauce);
